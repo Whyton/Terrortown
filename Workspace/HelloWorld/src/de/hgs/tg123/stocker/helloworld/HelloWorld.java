24ad3414292c8057dd9af181f53eca23117056b4
Whyton <Whyton@gmx.de>
2016-04-14 16:47:31 +0200

    Added files via upload

---
+++ Workspace/HelloWorld/src/de/hgs/tg123/stocker/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package de.hgs.tg123.stocker.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+			System.out.println("Wer das liest ist doof");
+	}
+
+}
---
+++ Workspace/Server/src/de/hgs/Terrortown/TerrortownServer.java
@@ -0,0 +1,11 @@
+package de.hgs.Terrortown;
+
+public class TerrortownServer {
+
+	public static void main(String[] args) {
+
+		
+	
+	}
+
+}
